c2f4aa53579d64c7661984285b9ebe401f1bb0d7
Tyler <tyler.s9@proton.me>
2025-08-01 20:15:11 -0700

    Restructuring && Express set-up: - Added an app.js with Express set-up - Added src/ -- src/public, src/views, and src/views - Additional adds: ejs templating, dotenv, PATH, and axios

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Set up EJS layouts
+app.use(expressLayouts);
+app.set('layout', 'layout');
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log('Server started at http://localhost:' + PORT);
+});
