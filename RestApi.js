14bcb87219e697e851141136301c3cc16581d53b
Jena L.R. White <jenale@gmail.com>
2025-08-03 23:03:26 -0400

    rest of API code, Google wont let me deploy

--- RestApi.js
+++ RestApi.js
@@ -6,6 +6,18 @@ function doGet(e) {
     const jobs = [];
 
     for (let i = 1; i < data.length; i++) {
+        //need to make a loop for the GET function to go through each row of data from Google Sheets, "JobBoard" sheet
+
+        const row = data[i];
+        for (let j = 0; j < headers.length; j++) {            
+            const job = {};
+            job[headers[j]] = row[j];
+            jobs.push(job);
+        }
+        
+        return ContentService.createTextOutput(JSON.stringify(jobs))
+            .setMimeType(ContentService.MimeType.JSON);
+            
 
     }
 
