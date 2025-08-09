b31d5d8757829453974bcc2a166e7fd3804f5809
Tyler <tyler.s9@proton.me>
2025-08-08 21:38:22 -0400

    Just adding more personal code logic, practising to merge the Google API data into the HTML on the Job Board. Taking a break -- about to have Long John Silvers xoxo

--- src/public/api-v2.js
+++ src/public/api-v2.js
@@ -4,9 +4,10 @@
  */
 async function loadIntoTable(url, table) {
     //use async to pause the code while fetch gets the data
-
     const tableHead = table.querySelector("thead");
     const tableBody = table.querySelector("tbody");
+    const response = await fetch(url);
+    const { headers, values } = await response.json();
 }
 
 loadIntoTable("./data.json", document.querySelector("table"));
